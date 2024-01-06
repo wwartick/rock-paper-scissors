c8ac826b0b34e8629300cca1101359697f5a02f7
wwartick <willwartick@gmail.com>
2024-01-06 15:01:22 -0800

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rock Paper Scissors
+The age old game of Rock Paper Scissors vs random selection.
+Built using HTML, CSS, Javascript
+Odin Project
---
+++ script.js
@@ -0,0 +1,14 @@
+/* functions from ODIN - getComputerChoice
+playRound function with 2 parameters - playerSelection and computerSelection
+ ^^ should return a STRING that declares the winner in "You Lose! Paper beats Rock." format
+game() function that contains the playRound function - meat and potatoes of the script.
+
+needs to read button selection from user - when button is pressed, also randomizes CPU selection
+change images of user selection to what user selected - do the same for CPU
+change instructions/win condition text to the round results
+first to 5 - displays a "play again" after either user/cpu wins + wipes scores and resets placeholder images
+account for ties? going to have to at some point in all of this
+
+*/
+ 
+  
