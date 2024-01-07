acc97608f0ecd57178ba049a3b06f7bc0b0aeff7
wwartick <willwartick@gmail.com>
2024-01-06 18:40:42 -0800

    scripts done

--- script.js
+++ script.js
@@ -7,8 +7,89 @@ needs to read button selection from user - when button is pressed, also randomiz
 change images of user selection to what user selected - do the same for CPU
 change instructions/win condition text to the round results
 first to 5 - displays a "play again" after either user/cpu wins + wipes scores and resets placeholder images
-account for ties? going to have to at some point in all of this
 
 */
+let playerWins=0;
+let computerWins=0;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+var gameOver = function(winner){
+    alert(winner + ' won! Press OK to play again.')
+    location.reload();
+}
+
+//function that determines the winner, returns string based on the result, adds a counter to the winner
+var playRound = function(playerSelection, computerSelection) {
+    document.getElementById('h3').innerHTML='';
+
+    if(playerSelection =='rock' && computerSelection == 'rock'){
+        document.getElementById('h2').innerHTML=`it's a tie! you both selected ${playerSelection}`;
+    } else if(playerSelection =='rock' && computerSelection == 'scissors'){
+        document.getElementById('h2').innerHTML=`you win! ${playerSelection} beats ${computerSelection}.`;
+        playerWins++;
+    }else if(playerSelection =='rock' && computerSelection == 'paper'){
+        document.getElementById('h2').innerHTML=`you lose! ${computerSelection} loses to ${playerSelection}.`;
+        computerWins++;
+    }
+
+    if(playerSelection =='paper' && computerSelection == 'paper'){
+         document.getElementById('h2').innerHTML=`it's a tie! you both selected ${playerSelection}`;
+    } else if(playerSelection =='paper' && computerSelection == 'rock'){
+        document.getElementById('h2').innerHTML=`you win! ${playerSelection} beats ${computerSelection}.`;
+        playerWins++;
+    }else if(playerSelection =='paper' && computerSelection == 'scissors'){
+        document.getElementById('h2').innerHTML=`you lose! ${computerSelection} loses to ${playerSelection}.`;
+        computerWins++;
+    }
+
+    if(playerSelection =='scissors' && computerSelection == 'scissors'){
+        document.getElementById('h2').innerHTML=`it's a tie! you both selected ${playerSelection}`;
+    } else if(playerSelection =='scissors' && computerSelection == 'paper'){
+        document.getElementById('h2').innerHTML=`you win! ${playerSelection} beats ${computerSelection}.`;
+        playerWins++;
+    }else if(playerSelection =='scissors' && computerSelection == 'rock'){
+        document.getElementById('h2').innerHTML=`you lose! ${computerSelection} loses to ${playerSelection}.`;
+        computerWins++;
+    }
+
+    document.getElementById('userScore').innerHTML=playerWins;
+    document.getElementById('computerScore').innerHTML=computerWins;
+
+    if(playerWins >=5){
+        gameOver('You');
+    }
+    else if(computerWins >=5){
+        gameOver('The computer')
+    }
+    
+
+}
+
+var game = function() {
+    let computerChoice;
+    //sets playerSelection to the pressed button
+    let userChoice=this.id;
+    //generates 0,1, or 2 randomly, and assigns a string value for the computers selection
+    let computerRandomizer=getRandomInt(3);
+    switch(computerRandomizer){
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2: 
+            computerChoice = "scissors";
+            break;
+    }
+    playRound(userChoice, computerChoice);
+}
  
+//tracks which button gets clicked 
+document.getElementById("rock").onclick = game;
+document.getElementById("paper").onclick = game;
+document.getElementById("scissors").onclick = game;
   
